ca405da29363dcdfee500074308bbd3234426a87
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-10-20 14:01:53 -0400

    Created the border and the turtle player

---
+++ space_invader.py
@@ -0,0 +1,44 @@
+import turtle
+import os
+
+#Set up the screen
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title("Space Invaders")
+
+# Draw a border
+border_pen = turtle.Turtle()
+border_pen.speed(0)
+border_pen.color('white')
+border_pen.penup()
+border_pen.setposition(-300,-300)
+border_pen.pendown()
+border_pen.pensize(3)
+for side in range(4):
+    border_pen.fd(600)
+    border_pen.lt(90)
+border_pen.hideturtle()
+
+# Create a player turtle 
+player = turtle.Turtle()
+player.color('blue')
+player.shape('triangle')
+player.penup()
+player.speed(0)
+player.setposition(0, -200)
+player.setheading(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+delay = input('Press enter to finsih')
