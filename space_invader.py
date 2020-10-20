73d519121b34ede97f7f9d96a735e957f1c49da3
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-10-20 15:22:18 -0400

    Use a global state called bulletstate and create a fucntion called fire_bullet. Used a conditional to make when the bulletstate is ready then bulletstate is going to = fire.

--- space_invader.py
+++ space_invader.py
@@ -41,6 +41,23 @@ enemy.setposition(-200,250)
 
 enemyspeed = 2
 
+# Create the players weapon 
+bullet = turtle.Turtle()
+bullet.color('yellow')
+bullet.shape('triangle')
+bullet.penup()
+bullet.speed(0)
+bullet.setheading(90)
+bullet.shapesize(0.5,0.5)
+bullet.hideturtle()
+
+bulletspeed = 20
+#Define bullet stae
+#ready - ready to fire
+# fire- bullet is firing
+bulletstate = 'ready'
+
+
 def move_left():
     # Takes the original corrdinate of x and subtract it by 15 to get x as the current position. 
     x = player.xcor()
@@ -56,10 +73,22 @@ def move_right():
         x = 280
     player.setx(x)
 
+def fire_bullet():
+    #Declare bulletsate as a global if it needs changed
+    global bulletstate
+    if bulletstate == 'ready':
+        bulletstate = 'fire'
+        # Move the bullet to the just above the player
+        x = player.xcor()
+        y = player.ycor() + 10
+        bullet.setposition(x , y)
+        bullet.showturtle()
+
 #create keyboard bindings
 turtle.listen()
 turtle.onkey(move_left, "Left")
-turtle.onkey(move_right, "Right")
+turtle.onkey(move_right, "Right") 
+turtle.onkey(fire_bullet, 'space')
 
 # Main game loop 
 while True:
@@ -78,6 +107,16 @@ while True:
         y -= 40
         enemy.sety(y)
         enemyspeed *= -1
+
+    #Move the bullet
+    if bulletstate == 'fire':
+        y = bullet.ycor()
+        y += bulletspeed
+        bullet.sety(y)
+    #Check to see if the bullet reached the top
+    if bullet.ycor() > 275:
+        bullet.hideturtle()
+        bulletstate = 'ready'
 
     
 
